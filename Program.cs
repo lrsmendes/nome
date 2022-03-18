772a0c077980131ef38f8877208a2eaf09e5e51f
Lucas Raziel <101843880+lrsmendes@users.noreply.github.com>
2022-03-17 21:09:24 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            // Console.Write("(c)Raziel, 2022\n");
+            Console.Write("Escreva o seu nome: ");
+            nome = Convert.ToString(Console.ReadLine());
+            Console.Write("\n");
+            Console.Write("O seu nome é: " + nome);
+            Console.Write("\n\nAperte uma tecla para sair...");
+            Console.ReadKey();
+        }
+    }
+}
